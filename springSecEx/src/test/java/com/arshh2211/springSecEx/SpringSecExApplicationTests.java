b47febe868f7263d4bc328b05687348b378bc5a6
Adarsh-1122-Arshh <adarsh1122arshh@gmail.com>
2025-06-23 17:17:07 +0530

    it's my first commit in Spring security project

---
+++ springSecEx/src/test/java/com/arshh2211/springSecEx/SpringSecExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arshh2211.springSecEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
